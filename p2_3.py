b90a09548c8d231e838e4adf447d0c0ec5959734
jishan0908-boop <jishankumar0908@gmail.com>
2024-11-06 09:52:49 +0530

    Add files via upload

---
+++ p2_3.py
@@ -0,0 +1,20 @@
+# c. Generate all the prime numbers till n:
+def isPrime(n):
+
+    if(n==1 or n==0):  return False
+  
+    for i in range(2,n):
+        if(n%i==0):
+            return False
+  
+    return True
+
+
+
+
+N = int(input("enter number till which prime numbers are to be generated:"))
+
+for i in range(1,N+1):
+  
+    if(isPrime(i)):
+        print(i,end=" ")
